95576aacc93cc3f8384e037df15ed922f805a11f
Chad Ohta <chadohta@uw.edu>
2019-12-03 21:09:08 -0800

    work on form

--- client/js/index.js
+++ client/js/index.js
@@ -3,22 +3,55 @@ formElem.addEventListener('submit', (e) => {
     // on form submission, prevent default
     e.preventDefault();
 
-    // construct a FormData object, which fires the formdata event
-    new FormData(formElem);
-});
+    // grab all form values
+    var company1 = document.getElementById("company").value;
+    var position1 = document.getElementById("positionID").value;
+    var salary1 = document.getElementById("salaryID").value;
+    var stipend1 = document.getElementById("userHousingStipend").value;
+    var stages1 = document.getElementById("userStages").value;
 
-formElem.addEventListener('formdata', (e) => {
-    console.log('formdata fired');
+    var process = []
+    if (document.getElementById("messageCheckbox1").checked) { 
+        process.push(document.getElementById("messageCheckbox1").value);
+    }
+    if (document.getElementById("messageCheckbox2").checked) {
+        process.push(document.getElementById("messageCheckbox2").value);
+    }
+    if (document.getElementById("messageCheckbox3").checked) {
+        process.push(document.getElementById("messageCheckbox3").value);
+    }
+    if (document.getElementById("messageCheckbox4").checked) {
+        process.push(document.getElementById("messageCheckbox4").value);
+    }
+    if (document.getElementById("messageCheckbox5").checked) {
+        process.push(document.getElementById("messageCheckbox5").value);
+    }
+    if (document.getElementById("messageCheckbox6").checked) {
+        process.push(document.getElementById("messageCheckbox6").value);
+    }
+    if (document.getElementById("messageCheckbox7").checked) {
+        process.push(document.getElementById("messageCheckbox7").value);
+    }
 
-    // Get the form data from the event object
-    // let data = e.formData;
-    // for (var value of data.values()) {
-    //     console.log(value);
-    // }
-    console.log(e.formData);
+    // construct body of request
+    var body = { 
+        "company": company1,
+        "position": position1,
+        "salary": salary1,
+        "stipened": stipend1,
+        "stages": stages1,
+        "process": process,
+    }
 
-    // submit the data via XHR
+    console.log(body)
+
+    // send request via API
     var request = new XMLHttpRequest();
-    request.open("POST", "https://99b87o7r17.execute-api.us-west-2.amazonaws.com/dev/createjobdetails");
-    request.send(formData);
+    request.open("POST", "https://a3gk63que0.execute-api.us-west-2.amazonaws.com/dev/createjobdetails");
+    request.send(body);
+
+    
+    // reset form
+    var form = document.getElementById("userForm");
+    form.reset();
 });
